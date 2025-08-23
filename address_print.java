527cbfdc972bb1701e012999e8d312defd45d4d7
Vishuuu193 <guptakaveen81@gmail.com>
2025-08-23 11:19:06 +0530

    address_print.java

---
+++ address_print.java
@@ -0,0 +1,11 @@
+public class address_multi_print {
+    public static void main(String[] args) {
+        System.out.println("MY");
+        System.out.println("ADRESS");
+        System.out.println("is");
+        System.out.println("gr.NOIDA");
+        System.out.println("UP");
+        System.out.println("INDIA");
+    }
+    
+}
